bbebf23fafb9dd1c039bd7d1be18ef838898a9c1
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2022-04-24 20:19:51 +1000

    bump version

--- src/action.js
+++ src/action.js
@@ -1,5 +1,16 @@
+const core = require('@actions/core');
+const github = require("@actions/github");
+
 async function run() {
-    console.log("Hello World!");
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+    const PACKAGE_PATH = core.getInput('PACKAGE_PATH');
+
+    const octokit = github.getOctokit(GITHUB_TOKEN);
+
+    const { context = {} } = github;
+    const { push } = context.payload
+
+    console.log(push)
 }
 
 run()
