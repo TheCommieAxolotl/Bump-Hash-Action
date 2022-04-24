2b1e9ff1dd90d5bd573fb55c87c1c76ecff10bfb
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2022-04-25 08:06:57 +1000

    bump version
    im more dumm

--- dist/index.js
+++ dist/index.js
@@ -1,38 +1,38 @@
-const core = require("@actions/core");
+const core = require('@actions/core');
 const github = require("@actions/github");
-const path = require("path");
-const fs = require("fs");
+const path = require('path')
+const fs = require('fs')
 const { createHash } = require("crypto");
 
 async function run() {
-    const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
-    const PACKAGE_PATH = core.getInput("PACKAGE_PATH");
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+    const PACKAGE_PATH = core.getInput('PACKAGE_PATH');
 
     const octokit = github.getOctokit(GITHUB_TOKEN);
-    const bumpRgx = /bump[\s]?(v|version)/gi;
+    const bumpRgx = /bump[\s]?(v|version)/gi
     const { context = {} } = github;
     const payload = context.payload;
 
     if (bumpRgx.test(payload.head_commit.message)) {
-        const pkjPath = path.join(__dirname, "../", "package.json");
-        if (!fs.existsSync(file.path)) return console.error("package.json not found");
-        const data = fs.readFileSync(pkjPath, "utf8");
+        const pkjPath = path.join(__dirname, "../", "package.json")
+        if (!fs.existsSync(pkjPath)) return console.error("package.json not found");
+        const data = fs.readFileSync(pkjPath, 'utf8')
 
-        let pkj = JSON.parse(data);
+        let pkj = JSON.parse(data)
 
         let newHash = createHash("sha512").update(data).digest("hex");
-        newHash = newHash.slice(0, 6);
+        newHash = newHash.slice(0, 6)
 
         if (typeof pkj.hash !== "undefined" && pkj.hash !== newHash) {
-            pkj.hash = newHash;
+            pkj.hash = newHash
             fs.writeFileSync(pkjPath, JSON.stringify(pkj, null, "\t"));
-            console.log("Updated package.json hash to:", newHash);
+            console.log("Updated package.json hash to:", newHash)
         } else if (typeof pkj.info.hash !== "undefined" && pkj.info.hash !== newHash) {
-            pkj.info.hash = newHash;
+            pkj.info.hash = newHash
             fs.writeFileSync(pkjPath, JSON.stringify(pkj, null, "\t"));
-            console.log("Updated package.json hash to:", newHash);
+            console.log("Updated package.json hash to:", newHash)
         }
     }
 }
 
-run();
+run()
